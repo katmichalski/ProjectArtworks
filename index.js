b110a0cb7214112b574bedd7a03337cdf1399992
Katiana Michalski <70670159+katmichalski@users.noreply.github.com>
2021-02-11 21:17:32 -0500

    fixed manifest file and fixed app to export the app

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.js'
-import registerServiceWorker from 'registerServiceWorker'
 import './index.css'
+import './manifest.json'
 
-var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!')
-ReactDOM.render(element, document.getElementById('root'))
-registerServiceWorker()
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
